6d082706cb66ab034e3bf34b7b46d1bdde853287
sarabiotto <sarabiotto@gmail.com>
2026-05-11 19:24:10 -0300

    Create list.py

---
+++ list.py
@@ -0,0 +1,7 @@
+class Musica:
+    def __init__(self, id, titulo, artista, genero, bpm):
+
+class NodoLista:
+    def __init__(self, musica):
+        self.musica = musica
+        self.proximo = None
