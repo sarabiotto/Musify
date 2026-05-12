024e17ea0f3e34f93ca3a1c19cdb7f0852abfbe8
sarabiotto <sarabiotto@gmail.com>
2026-05-11 23:11:05 -0300

    Update list.py

--- list.py
+++ list.py
@@ -9,5 +9,20 @@ class Musica:
     def __str__(self):
         return (f"[{self.id}] {self.titulo} - {self.artista} "
                 f"| Gênero: {self.genero} | BPM: {self.bpm}")
-m = Musica(1, "Faroeste Caboclo", "Legião Urbana", "Rock", 128)
-print(m)
+    
+class NodoLista:
+    def __init__(self, musica):
+        self.musica = musica
+        self.proximo = None
+    
+m1 = Musica(1, "Faroeste Caboclo", "Legião Urbana", "Rock", 128)
+m2 = Musica(2, "Se", "Djavan", "MPB", 76)
+
+n1 = NodoLista(m1)
+n2 = NodoLista(m2)
+
+n1.proximo = n2  
+
+print(n1.musica)        
+print(n1.proximo.musica)
+
